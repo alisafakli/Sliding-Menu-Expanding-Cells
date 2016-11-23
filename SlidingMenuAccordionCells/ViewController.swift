70a1ca37ab1a2f78aa5eb906a853619a743f96e3
Ali Safakli <alisafakli@Alis-MacBook-Pro.local>
2016-11-23 20:04:01 +0300

    First Commit

---
+++ SlidingMenuAccordionCells/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SlidingMenuAccordionCells
+//
+//  Created by Ali Safakli on 23/11/2016.
+//  Copyright © 2016 Wonderland. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
